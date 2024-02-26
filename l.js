fe94244d6e8b6c268cc33c59d3be359384a0b740
Ann <annaelfimova8a@yandex.ru>
2024-02-26 14:39:17 +0300

    348

---
+++ l.js
@@ -0,0 +1,234 @@
+// let p = document.querySelectorAll('p');
+// let span = document.querySelector('span');
+// let div = document.querySelector('d1');
+// for(let elem of p) {
+//     elem.addEventListener('click', ()=> {
+//         elem.style.color='green';
+//     })
+
+// }
+// let button = document.querySelector('#button');
+// console.log(button);
+
+// let elem1 = document.querySelector('#elem1');
+// console.log(elem1);
+// let elem2 = document.querySelector('#elem2');
+// console.log(elem2);
+// let elem3 = document.querySelector('#elem3');
+// console.log(elem3);
+// let el = document.querySelector('#block p');
+// console.log(el);
+// let el1 = document.querySelector('.block p');
+// console.log(el1);
+// let t = document.querySelector('.www');
+// console.log(t);
+
+// let button1 = document.querySelector('#button1');
+// button1.addEventListener('click', function() {
+// 	console.log('1');
+// });
+
+// let button2 = document.querySelector('#button2');
+// button2.addEventListener('click', function() {
+// 	console.log('2');
+// });
+
+// let button3 = document.querySelector('#button3');
+// button3.addEventListener('click', function() {
+// 	console.log('3');
+// });
+
+
+// let button1 = document.querySelector('#button1');
+// button1.addEventListener('click', func1);
+
+// let button2 = document.querySelector('#button2');
+// button2.addEventListener('click', func2);
+// function func1() {
+// 	console.log(1);
+// }
+
+// function func2() {
+// 	console.log(2);
+// // }
+// function func() {
+// 	console.log('message');
+// }
+
+// let elem1 = document.querySelector('#elem1');
+// elem1.addEventListener('click', func);
+
+// let elem2 = document.querySelector('#elem2');
+// elem2.addEventListener('click', func);
+// let elem3 = document.querySelector('#elem3');
+// elem3.addEventListener('click', func);
+// let elem4 = document.querySelector('#elem4');
+// elem4.addEventListener('click', func);
+// let elem5 = document.querySelector('#elem5');
+// elem5.addEventListener('click', func);
+
+// function func1() {
+// 	console.log('1');
+// }
+
+// function func2() {
+// 	console.log('2');
+// }
+
+// function func3() {
+// 	console.log('3');
+// }
+// let elem = document.querySelector('#elem');
+
+// elem.addEventListener('click', func1);
+// elem.addEventListener('click', func2);
+// elem.addEventListener('click', func3);
+
+// let button = document.querySelector('#button');
+// button.addEventListener('dblclick', function() {
+// 	console.log('1');
+// });
+
+// let button = document.querySelector('#button');
+// button.addEventListener('mouseover', function() {
+// 	console.log('1');
+// });
+
+// let button = document.querySelector('#button');
+// button.addEventListener('mouseout', function() {
+// 	console.log('1');
+// });
+
+// let button = document.querySelector('#button');
+// button.addEventListener('mouseover', function() {
+// 	console.log('1');
+// });
+// button.addEventListener('mouseout', function() {
+// 	console.log('2');
+// });
+
+// button.addEventListener('click', function() {
+//     console.log(elem.textContent);
+// });
+
+// button.addEventListener('click', function() {
+//     elem.textContent = '!!!';
+// });
+
+// button.addEventListener('click', function() {
+//     let a = Number(elem1.textContent);
+//     let b = Number(elem2.textContent);
+//     console.log(a + b);
+// });
+
+//346.4
+// let button = document.querySelector('#button');
+// let d = document.querySelector('#d');
+// button.addEventListener('click', function() {
+//     let a = Number(elem1.textContent);
+//     let b = Number(elem2.textContent);
+//     let c = Number(elem3.textContent);
+//     d.textContent = (a + b + c);
+// });
+
+// let button = document.querySelector('#button');
+
+// let p = document.querySelector('#elem');
+
+// button.addEventListener('click', function() {
+//     let a = Number(elem.textContent);
+//     elem.textContent = (a + 1);
+// });
+
+// let button = document.querySelector('#button');
+
+// let p = document.querySelector('#elem');
+
+// button.addEventListener('click', function() {
+//     let a = elem.textContent;
+//     elem.textContent = (a + "!");
+// });
+
+// let button = document.querySelector('#button');
+// let p = document.querySelector('#elem');
+// button.addEventListener('click', function() {
+//     console.log(p.innerHTML);
+// });
+
+// let button = document.querySelector('#button');
+// let p = document.querySelector('#elem');
+// button.addEventListener('click', function() {
+//     p.innerHTML = "<b>ff</b>";
+// });
+
+// let elem = document.querySelector('#elem');
+// let button = document.querySelector('#button');
+// button.addEventListener('click', function() {
+//     console.log(elem.type);
+// });
+
+// let elem = document.querySelector('#elem');
+// let button = document.querySelector('#button');
+// button.addEventListener('click', function() {
+//     elem.type = 'submit';
+// });
+
+//348.3
+// let link = document.querySelector('a');
+// let button = document.querySelector('#button');
+// let p = document.querySelector('p');
+
+// button.addEventListener('click', function() {
+//     p.innerHTML = link.href;
+// });
+
+//348.4
+// let link = document.querySelector('a');
+// let button = document.querySelector('#button');
+// let p = document.querySelector('p');
+
+// button.addEventListener('click', function() {
+//     link.textContent +=  '(' + link.href + ')';
+// });
+
+//348.5
+// let link = document.querySelector('img');
+// let button = document.querySelector('#button');
+// let p = document.querySelector('p');
+
+// button.addEventListener('click', function() {
+//     p.innerHTML = link.src;
+// });
+
+//348.6
+// let link = document.querySelector('img');
+// let button = document.querySelector('#button');
+// let p = document.querySelector('p');
+
+// button.addEventListener('click', function() {
+//     link.width = 300;
+// });
+
+//348.7
+// let link = document.querySelector('img');
+// let button = document.querySelector('#button');
+// let p = document.querySelector('p');
+
+// button.addEventListener('click', function() {
+//     link.width = +link.width * 2;
+// });
+
+//348.8
+// let link = document.querySelector('img');
+// let link2 = document.querySelector('img');
+
+// let button = document.querySelector('#button1');
+// let button2 = document.querySelector('#button');
+
+// button.addEventListener('click', function() {
+//     link2.src = "Без имени-5.png"; 
+// });
+// button2.addEventListener('click', function() {
+//     link.src = "Frame 1.jpg"; 
+// });
+
